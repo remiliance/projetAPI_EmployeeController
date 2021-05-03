2325cc48ae7700774d81c6524561c657dd5c0e24
remiliance <56579705+remiliance@users.noreply.github.com>
2021-05-03 18:07:35 +0200

    Update Jenkinsfile.groovy

---
+++ api/Jenkinsfile.groovy
@@ -0,0 +1,33 @@
+node {
+  try{
+    def buildNum = env.BUILD_NUMBER 
+    def branchName= env.BRANCH_NAME
+    
+    print buildNum
+    print branchName
+
+    def registryProjet='registry.gitlab.com/remiliance/registrydockerimage'
+    def IMAGE="${registryProjet}:version-${env.BUILD_ID}"
+   
+   stage('Clone') {
+        git 'https://github.com/remiliance/projet_panier_produit.git'
+    
+    }
+    stage('Build Maven') {
+        echo 'debut du build'
+        sh 'mvn -DskipTests=true package' 
+        docker.build("$IMAGE", '.')
+    }
+   
+   def imageName='192.168.5.5:5000/api'
+   
+   stage('Push') {
+      docker.withRegistry('http://192.168.5.5:5000', 'myregistry_login') {
+         def customImage = docker.build("$imageName")
+         customImage.push()
+      }
+  
+   } finally {
+    cleanWs()
+  }
+}
