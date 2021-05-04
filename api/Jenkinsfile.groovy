24cb13a85e21a6bcd56308a61efe3d5ed9948c67
remiliance <56579705+remiliance@users.noreply.github.com>
2021-05-04 10:32:40 +0200

    Update Jenkinsfile.groovy

--- api/Jenkinsfile.groovy
+++ api/Jenkinsfile.groovy
@@ -16,15 +16,37 @@ node {
    dir('api') {
    stage('Build Maven') {
         
+   /* déterminer l'extension */
+    if (branchName == "dev" ){
+      extension = "-SNAPSHOT"
+    }
+    if (branchName == "stage" ){
+      extension = "-RC"
+    }
+    if (branchName == "master" ){
+      extension = ""
+    }
+
+    /* Récupération du commitID long */
+    def commitIdLong = sh returnStdout: true, script: 'git rev-parse HEAD'
+
+    /* Récupération du commitID court */
+    def commitId = commitIdLong.take(7)
+
+    /* Modification de la version dans le pom.xml */
+    sh "sed -i s/'-XXX'/${extension}/g pom.xml"
+
     /* Récupération de la version du pom.xml après modification */
-    def version = sh returnStdout: true, script: "cat pom.xml | grep -A1 '<artifactId>api1' | tail -1 |perl -nle 'm{.*<version>(.*)</version>.*};print \$1' | tr -d '\n'"
+    def version = sh returnStdout: true, script: "cat pom.xml | grep -A1 '<artifactId>api' | tail -1 |perl -nle 'm{.*<version>(.*)</version>.*};print \$1' | tr -d '\n'"
 
      print """
      #################################################
         BanchName: $branchName
+        CommitID: $commitId
         AppVersion: $version
+        JobNumber: $buildNum
      #################################################
-        """    
+
     
         echo 'debut du build'
         sh 'mvn package -Dmaven.test.skip=true' 
